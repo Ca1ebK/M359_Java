c7656c69cab43108fee1e8af9246f72017083e9f
Caleb J Kang <kang1693@students.d211.org>
2023-09-18 14:36:55 -0500

    Created pizza object

---
+++ src/U3_BooleanAndIfStatements/ClassNotes/PizzaTester.java
@@ -0,0 +1,7 @@
+package U3_BooleanAndIfStatements.ClassNotes;
+
+public class PizzaTester {
+    public static void main(String[] args) {
+        Pizza monPizza = new Pizza("Costco", 7, 2, false);
+    }
+}
