a28d4ad35e1df6dca9e2e1a95173aabb05b4c43b
Ca1ebK <caleb.jpkang@gmail.com>
2023-09-18 15:36:46 -0500

    Made a change from home

--- src/U3_BooleanAndIfStatements/ClassNotes/PizzaTester.java
+++ src/U3_BooleanAndIfStatements/ClassNotes/PizzaTester.java
@@ -3,5 +3,7 @@ package U3_BooleanAndIfStatements.ClassNotes;
 public class PizzaTester {
     public static void main(String[] args) {
         Pizza monPizza = new Pizza("Costco", 7, 2, false);
+        // I made a change from home! 3:35 PM September 18, 2023
     }
+
 }
