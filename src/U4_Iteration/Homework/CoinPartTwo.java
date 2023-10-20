d1aaad67c6b3a12296f7a44190b96d7b25709694
Caleb J Kang <kang1693@students.d211.org>
2023-10-20 14:43:08 -0500

    Finished Coin Flip MP! :100:

--- src/U4_Iteration/Homework/CoinPartTwo.java
+++ src/U4_Iteration/Homework/CoinPartTwo.java
@@ -23,31 +23,14 @@ public class CoinPartTwo {
 
         for (int i = 0; i < numPlays; i++) {
 
-            previousCoin = myCoin.isHeads();
             System.out.println(myCoin);
-            myCoin.flip();
-
-            if (i == 0) {
-                if (previousCoin) {
-                    currentHeadStreak = 1;
-                }
-                else {
-                    currentTailStreak = 1;
-                }
+            if (myCoin.isHeads()) {
+                currentHeadStreak++;
+                currentTailStreak = 0;
             }
             else {
-                if (myCoin.isHeads() && previousCoin) { // if it's heads and heads
-                    currentHeadStreak++;
-                }
-                else if (myCoin.isHeads() && !previousCoin) { // if it's heads and tails
-                    currentHeadStreak = 1;
-                }
-                else if (!myCoin.isHeads() && !previousCoin) { // if it's tails and tails
-                    currentTailStreak++;
-                }
-                else { // if it's tails and heads
-                    currentTailStreak = 1;
-                }
+                currentTailStreak++;
+                currentHeadStreak = 0;
             }
             if (currentHeadStreak > maxHeadStreak) {
                 maxHeadStreak = currentHeadStreak;
@@ -55,12 +38,14 @@ public class CoinPartTwo {
             if (currentTailStreak > maxTailStreak) {
                 maxTailStreak = currentTailStreak;
             }
+            myCoin.flip();
+
         }
 
-        System.out.println("Current head streak: " + currentHeadStreak);
         System.out.println("Maximum head streak: " + maxHeadStreak);
         System.out.println("Maximum tail streak: " + maxTailStreak);
 
 
 
-}}
+    }
+}
