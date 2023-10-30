1d9a81950931c8bff05429f4966f3b506c962120
Ca1ebK <caleb.jpkang@gmail.com>
2023-10-29 21:37:52 -0500

    Update Test.java

--- src/U5_WritingClasses/ClassNotes/Test.java
+++ src/U5_WritingClasses/ClassNotes/Test.java
@@ -1,18 +1,48 @@
 package U5_WritingClasses.ClassNotes;
 
 public class Test {
-    public static int countWords(String str) {
-        int loc = str.indexOf(" ");
-        String spaces = "";
-        while (loc != -1) {
-            spaces += str.charAt(loc);
-            str = str.substring(loc + 1);
-            // loc = str.indexOf(" ");
+    public static String mirrorEnds(String string) {
+        String reverseString = "";
+        String newString = "";
+        for (int i = string.length() - 1; i >= 0; i--) {
+            reverseString += string.charAt(i);
+            for (int j = 0; j < string.length(); j++) {
+                if (reverseString.equals(string.substring(0, j))) {
+                    newString += string.charAt(i);
+                }
+            }
         }
-        return spaces.length() + 1;
+
+        return newString;
     }
 
+    public static String notReplace(String str) {
+
+        String newStr = "";
+
+        for (int i = 0; i < str.length(); i++) {
+//            char myChar = str.charAt(i);
+//
+//            if (Character.isLetter(myChar)) {
+//                newStr += "True";
+//            }
+            if (str.substring(0, i).equals("is")) {
+                newStr += "True";
+            }
+            for (int j = i + 1; j < str.length() - i; j++) {
+                char myChar = str.charAt(j);
+                if (Character.isLetter(myChar)) {
+                    newStr += "False";
+                }
+            }
+        }
+
+        return newStr;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(countWords("This is a Java Test"));
+        // System.out.println(mirrorEnds("abXYZba"));
+        System.out.println(notReplace("isis"));
     }
 }
