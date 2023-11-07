50316819e2e8006e614baf980ba01ac33482075d
Caleb J Kang <kang1693@students.d211.org>
2023-11-07 14:42:28 -0600

    Update Marvel Class

--- src/U5_WritingClasses/MarvelEx/MarvelMovieTester.java
+++ src/U5_WritingClasses/MarvelEx/MarvelMovieTester.java
@@ -3,19 +3,30 @@ package U5_WritingClasses.MarvelEx;
 public class MarvelMovieTester {
     public static void main(String[] args) {
         // className var = new className( ... params ...);
-        Power webslinger = new Power("Slings webs", 7);
+        Power webslinger = new Power("Slings webs", 80);
         MarvelCharacter spiderman = new MarvelCharacter("Spiderman",
                 webslinger, true);
+        MarvelCharacter milesMorales = new MarvelCharacter("Miles Morales", webslinger, true);
 
-        Power grenades = new Power("Pew pew - kaboom", 3);
+
+        Power grenades = new Power("Pew pew - kaboom", 40);
         MarvelCharacter greenGoblin = new MarvelCharacter("Green goblin",
                 grenades, false);
 
         System.out.println(spiderman);
-        System.out.println(greenGoblin);
+        // System.out.println(greenGoblin);
+        System.out.println(milesMorales);
 
-        battle(spiderman, greenGoblin);
+        int amtToAdd = 15;
+        changePower(webslinger, amtToAdd);
+        System.out.println("Adding " + amtToAdd);
+        // battle(spiderman, greenGoblin);
 
+        System.out.println(spiderman);
+        // System.out.println(greenGoblin);
+        System.out.println(milesMorales);
+
+        System.out.println("New value is: " + amtToAdd);
     }
 
     public static void battle(MarvelCharacter char1, MarvelCharacter char2) {
@@ -27,6 +38,12 @@ public class MarvelMovieTester {
 
         System.out.println(char1.getName() + " Health: " + char1.getHealth());
         System.out.println(char2.getName() + " Health: " + char2.getHealth());
+    }
+
+    public static void changePower(Power p, int val) {
+        int oldPower = p.getPower();
+        val++;
+        p.setPower(oldPower + val);
     }
 
 }
