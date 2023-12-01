8e12b1ecafc2e694138926bd5a5191044e7b3384
Caleb J Kang <kang1693@students.d211.org>
2023-12-01 14:44:36 -0600

    Update StudentTester.java

--- src/U6_Arrays/CourseStudentExample/StudentTester.java
+++ src/U6_Arrays/CourseStudentExample/StudentTester.java
@@ -25,14 +25,14 @@ public class StudentTester {
         // System.out.println(caleb);
 
 
-        Scanner inF = new Scanner(new File("oneCourse.txt"));
-        String teacher = inF.nextLine();
-        String className = inF.nextLine();
-        String grade = inF.nextLine();
-        int period = inF.nextInt();
-
-        Course C = new Course(teacher, className, grade, period);
-        System.out.println(C);
+//        Scanner inF = new Scanner(new File("oneCourse.txt"));
+//        String teacher = inF.nextLine();
+//        String className = inF.nextLine();
+//        String grade = inF.nextLine();
+//        int period = inF.nextInt();
+//
+//        Course C = new Course(teacher, className, grade, period);
+//        System.out.println(C);
 
         // Make an array of type Course to save all the
         // course data
@@ -48,12 +48,71 @@ public class StudentTester {
             String myClass = inSchedule.nextLine();
             String myGrade = inSchedule.nextLine();
             int myPeriod = inSchedule.nextInt();
+            // The EOL char after the number for the period
+            // isn't 'consumed'. We need to get rid of it!
+
+            // check if there is another line before we do the dummy read
+            if (inSchedule.hasNextLine()) {
+                // Do a dummy read!!!!
+                inSchedule.nextLine();
+            }
+
+
             Course myCourse = new Course(myTeacher, myClass, myGrade, myPeriod);
             schedule[i] = myCourse;
         }
 
         // Fix the run time error
         // close the Scanner objects
+
+        inSchedule.close();
+        Student s = new Student(studentName, schedule);
+        System.out.println(s);
+
+        /////////////////////////////////////////////////////////////////
+
+        Scanner allSchedules = new Scanner(new File("studentScheduleData.txt"));
+        int studentMaxSize = allSchedules.nextInt();
+        // need a dummy read if we are reading a nextLine after a nextInt
+        allSchedules.nextLine();
+
+        Student[] allStudents = new Student[studentMaxSize];
+        int index = 0; // this is where the new student is added in addStudents
+
+        // continue as long as there is more data
+        while (allSchedules.hasNextLine()) {
+            // each time will read one student
+            // complete the process to read in ONE student
+            String name = allSchedules.nextLine();
+
+            Course[] oneSchedule = new Course[8];
+            // Use a loop to read in all 8 sets of Course data
+            for (int i = 0; i < schedule.length; i++) {
+                String myTeacher = allSchedules.nextLine();
+                String myClass = allSchedules.nextLine();
+                String myGrade = allSchedules.nextLine();
+                int myPeriod = allSchedules.nextInt();
+                // The EOL char after the number for the period
+                // isn't 'consumed'. We need to get rid of it!
+
+                // check if there is another line before we do the dummy read
+                if (allSchedules.hasNextLine()) {
+                    // Do a dummy read!!!!
+                    allSchedules.nextLine();
+                }
+                Course myCourse = new Course(myTeacher, myClass, myGrade, myPeriod);
+                oneSchedule[i] = myCourse;
+            }
+            // Creating a Student object with the name and Course []
+            Student myStudent = new Student(name, oneSchedule);
+            allStudents[index] = myStudent;
+            index++;
+
+        }
+
+        for (int i = 0; i < allStudents.length; i++) {
+            System.out.println(allStudents[i]);
+        }
 
     }
 }
