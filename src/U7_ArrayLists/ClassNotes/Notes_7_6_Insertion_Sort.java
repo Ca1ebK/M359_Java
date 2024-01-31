7b1b704fec5d77af716d025b01bdffbafc6d57d7
Caleb J Kang <kang1693@students.d211.org>
2024-01-31 14:44:50 -0600

    Worked on Implementing Insertion Sort

--- src/U7_ArrayLists/ClassNotes/Notes_7_6_Insertion_Sort.java
+++ src/U7_ArrayLists/ClassNotes/Notes_7_6_Insertion_Sort.java
@@ -10,16 +10,28 @@ public class Notes_7_6_Insertion_Sort {
         for (int i = 1; i < nums.length; i++) {
             // make a temp var to save the value that will be inserted
             int temp = nums[i];
+            int j = i - 1;      // start comparing one value to the left
             // while loop checks two things!
             // first, verify there is still a value to compare with
             // check if need to slide the value over
-
-            // var represents the index we are checking
-            int j = i - 1;
-            // while loop!
+            while (j >= 0 && temp < nums[j]) {
+                // slide the value at j over right
+                nums[j + 1] = nums[j];
+                // move j one to the left to keep checking
+                j--;
+            }
+            // insert the value back in
+            nums[j + 1] = temp;
         }
-    }
 
+        printArr(nums);
+
+        String[] names = {"Yusr", "Jeff", "Ashwin", "Aditya", "Gavin"};
+        printArr(names);
+        insertionSortStrings(names);
+        printArr(names);
+
+    }
     public static void printArr(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
@@ -27,4 +39,29 @@ public class Notes_7_6_Insertion_Sort {
         System.out.println();
     }
 
+    public static void printArr(String[] arr) {
+        for (String i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void insertionSortStrings(String[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            // make a temp var to save the value that will be inserted
+            String temp = arr[i];
+            int j = i - 1;      // start comparing one value to the left
+            // while loop checks two things!
+            // first, verify there is still a value to compare with
+            // check if need to slide the value over
+            while (j >= 0 && temp.compareTo(arr[j]) < 0) {
+                // slide the value at j over right
+                arr[j + 1] = arr[j];
+                // move j one to the left to keep checking
+                j--;
+            }
+            // insert the value back in
+            arr[j + 1] = temp;
+        }
+    }
 }
